e6561778bb092036205a2ccfdcec39efe7d25883
zzorin <ilya-zorin@yandex.ru>
2019-08-11 22:48:19 +0700

    deleted logs from record_component

--- recordstore-front/src/components/records/Records.vue
+++ recordstore-front/src/components/records/Records.vue
@@ -52,7 +52,6 @@
             <svg class="fill-current text-indigo w-6 h-6 mr-2" viewBox="0 0 24 24" width="24" height="24"><title>record vinyl</title><path d="M23.938 10.773a11.915 11.915 0 0 0-2.333-5.944 12.118 12.118 0 0 0-1.12-1.314A11.962 11.962 0 0 0 12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12c0-.414-.021-.823-.062-1.227zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-5a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" ></path></svg>
             {{ record.title }} &mdash; {{ record.year }}
           </p>
-          {{record}}
           <p class="block font-mono font-semibold">{{ getArtist(record) }}</p>
         </div>
 
@@ -124,7 +123,6 @@ export default {
     },
     getArtist (record) {
       const recordArtistValues = this.artists.filter(artist => artist.id === record.artist_id)
-      console.log(record)
       let artist
 
       recordArtistValues.forEach(function (element) {
